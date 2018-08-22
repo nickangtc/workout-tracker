8a344b56fb2334ac68609daa7de1a6f21ebf441d
Nick Ang <nickangtc@gmail.com>
2018-08-22 15:23:20 +0800

    Can POST data to server

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ const bodyParser = require('body-parser');
 
 // Configurations
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
 // Routes
 app.get('/', function (req, res) {
     console.log('GET / request received');
     res.render('index.ejs');
+});
+
+app.post('/exercises', function (req, res) {
+    console.log('POST /exercises request received');
+    res.json(req.body);
+    // res.render('index.ejs');
 });
 
 // Run server!
