527d9b2706dc85b9ed6c601f1df0fd42377826d7
Nick Ang <nickangtc@gmail.com>
2018-09-15 11:44:05 +0800

    Fix morgan module error in production

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const sequelize = require('sequelize');
 const momentjs = require('moment');
 
 // Configurations
-app.use(require('morgan')('dev'));
+if (app.get('env') === 'development') {
+    app.use(require('morgan')('dev'));
+}
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());       // to support JSON-encoded bodies
